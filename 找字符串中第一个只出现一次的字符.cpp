e8a4761cdd090f7c3689367dbd9dc4c8648e857e
southern-yan <468137075@qq.com>
2019-03-31 08:48:42 +0800

    找字符串中第一个只出现一次的字符

---
+++ 找字符串中第一个只出现一次的字符.cpp
@@ -0,0 +1,34 @@
+
+//
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s;
+//	while (getline(cin, s))
+//	{
+//		int countarray[256] = { 0 };
+//		for (size_t i = 0; i<s.size(); i++)
+//		{
+//			countarray[s[i]]++;
+//		}
+//		bool flag = true;
+//		for (size_t i = 0; i<s.size(); i++)
+//		{
+//			if (countarray[s[i]] == 1)
+//			{
+//				cout << s[i] << endl;
+//				flag = false;
+//				break;
+//			}
+//
+//		}
+//		if (flag)
+//			cout << -1 << endl;
+//	}
+//
+//}
+
