5a9e049786cfec51bb02c9254ba1b7747dc5ca9f
southern-yan <468137075@qq.com>
2019-04-18 07:45:02 +0800

    查找两个字符串a,b中的最长公共子串

---
+++ 查找两个字符串a,b中的最长公共子串.cpp
@@ -0,0 +1,45 @@
+//题目：
+//查找两个字符串a,b中的最长公共子串。若有多个，输出在较短串中最先出现的那个。
+//输入描述:输入两个字符串
+//输出描述:返回重复出现的字符
+//示例1
+//输入：abcdefghijklmnop
+            abcsafjklmnopqrstuvw
+//输出：jklmnop
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    while(cin>>s1>>s2)
+    {
+        if(s1.size()<s2.size())
+        {
+        swap(s1,s2);
+        }
+        string ret;
+        for(int i=0;i<s2.size();i++)
+        {
+           for(int j=i;j<s2.size();j++)
+           {
+               string tmp=s2.substr(i,j-i+1);
+               if(int(s1.find(tmp))<0)
+               {
+                   break;
+               }
+               else if(ret.size()<tmp.size())
+               {
+                  ret=tmp;
+               }
+           }
+        }
+        cout<<ret<<endl;
+     }
+      return 0;
+}
