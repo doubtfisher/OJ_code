5322b35f69997cb5fa0e7209d8ec873548cb144d
southern-yan <468137075@qq.com>
2019-05-28 22:59:51 +0800

    fibonacci数的应用之跳方格

---
+++ 跳方格.cpp
@@ -0,0 +1,25 @@
+//ÌâÄ¿£º
+
+
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		int a = 0;
+//		int b = 1;
+//		int c;
+//		while (n--)
+//		{
+//			c = a + b;
+//			a = b;
+//			b = c;
+//		}
+//		cout << c << endl;
+//	}
+//	return 0;
+//}
