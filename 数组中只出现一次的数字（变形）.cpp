ceb64d5b6e4613e8ece77885dc8bff6ce7dbc02a
southern-yan <468137075@qq.com>
2019-09-16 22:18:19 +0800

    新增一种方法

--- 数组中只出现一次的数字（变形）.cpp
+++ 数组中只出现一次的数字（变形）.cpp
@@ -74,7 +74,28 @@ public:
 };
 
 （2）
+将每个数想象成32位的二进制，对于每一位的二进制的1和0累加起来必然是3N或者3N+1， 为3N代表目标值在这一位没贡献，3N+1代表目标值在这一位有贡献(=1)，然后将所有有贡献的位|起来就是结果。这样做的好处是如果题目改成K个一样，只需要把代码改成cnt%k，很通用。
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret=0;
+        for(int i=0;i<32;i++)
+        {
+            int mask=1<<i;//取出数中每一位
+            int cnt=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if((nums[j]&mask)!=0)//数据的某一位为1
+                    cnt++;        
+            }
+            if(cnt%3!=0)
+                ret|=mask;
+        }
+         return ret;
+    }
+  
+};
        
         }
         return res;
