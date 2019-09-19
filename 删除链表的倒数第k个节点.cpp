b4d93c03023c0220dcb7ea66ca4af9967b089596
southern-yan <468137075@qq.com>
2019-09-19 23:00:10 +0800

    删除链表的倒数第k个节点

---
+++ 删除链表的倒数第k个节点.cpp
@@ -0,0 +1,39 @@
+题目：
+输入一个链表，输出该链表中倒数第k个结点。
+
+思路：
+先让快指针走k步，然后再让快慢指针同时走，当快指针走到最后一个节点时，slow的下一个节点就是要删除的节点。
+
+代码实现：
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = nullptr;
+        int i = 0;
+        while(fast)
+        {
+            if(i==n)//先让fast走n步
+                break;
+            ++i;
+            fast = fast->next;
+        }
+       
+        if(i==n &&fast==nullptr) 
+            return head->next; //删的是头结点
+        
+        if(i!=n)  //如果实际从while语句中跳出来，就说明要删除的倒数第n个数不存在，直接返回头结点
+            return head; 
+        
+        slow = head;
+        while(fast->next)//如果判断fast不为空的话，会把要删除的节点错过
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        slow->next = slow->next->next;
+        return head;
+    }
+};
